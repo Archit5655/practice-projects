5bddf6e536cec70431aa22eaf95cb8e0b0734387
Archit5655 <architgargagra@gmail.com>
2025-08-03 03:02:59 +0530

    added  svg

--- gradient-mask/gradient-mask/app/page.tsx
+++ gradient-mask/gradient-mask/app/page.tsx
@@ -1,10 +1,27 @@
 import Gradient from './components/Gradient-Mask';
 export default function Home() {
   return (
+
   <>
- <Gradient/> 
+
+
+
+
+
+  <div className="relative inline-block">
+  <div className="bg-gray-400 text-white text-base px-4 py-2 rounded-xl text-center">
+    Helper Text
+  </div>
+  <div className="absolute left-1/2 -bottom-2 transform -translate-x-1/2 w-0 h-0 border-x-8 border-x-transparent border-t-8 border-t-gray-400"></div>
+</div>
+
+ {/* <Gradient/>  */}
+ {/* <div>
+
+  <h1 className=' '>MY nam eis arehcit </h1>
+ </div>
  {/* <GridBackGRound/> */}
- <div>{}</div>
+ {/* <div>{}</div> */} 
   </>
   );
 }
